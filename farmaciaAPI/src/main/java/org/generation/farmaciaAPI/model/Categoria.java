f9fa56064c1555933efd90f4aa3d09c0ac3631d1
EwertonILima <ewertoninaciolima@gmail.com>
2021-02-05 21:09:11 -0300

    Criado relacionamento entre tb_produto e tb_categoria

--- farmaciaAPI/src/main/java/org/generation/farmaciaAPI/model/Categoria.java
+++ farmaciaAPI/src/main/java/org/generation/farmaciaAPI/model/Categoria.java
@@ -1,12 +1,18 @@
 package org.generation.farmaciaAPI.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_categoria")
@@ -19,10 +25,15 @@ public class Categoria {
 	@NotNull
 	@Size(min = 3, max = 50)
 	private String nome;
-
+	
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
+	
 	public Long getId() {
 		return id;
 	}
+
 
 	public void setId(Long id) {
 		this.id = id;
@@ -34,6 +45,14 @@ public class Categoria {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
 	}
 	
 	
--- farmaciaAPI/src/main/java/org/generation/farmaciaAPI/model/Produto.java
+++ farmaciaAPI/src/main/java/org/generation/farmaciaAPI/model/Produto.java
@@ -4,10 +4,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_produto")
@@ -31,6 +34,10 @@ public class Produto {
 	@NotNull
 	@Size(max = 50)
 	private String fornecedor;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
 
 	public long getId() {
 		return id;
@@ -70,6 +77,14 @@ public class Produto {
 
 	public void setFornecedor(String fornecedor) {
 		this.fornecedor = fornecedor;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
 	}
 	
 	
